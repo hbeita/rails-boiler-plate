181f960cb34dc08d9c99c1bf27a16a601ff8ec31
Hector-Beita <hector.garcia@shareworks.com>
2021-04-20 08:45:17 -0600

    WIP added vue and veutify (not functional)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,4 +4,34 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+
+environment.config.merge({
+  module: {
+    rules: [
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            // Requires sass-loader@^7.0.0
+            options: {
+              implementation: require('sass'),
+              indentedSyntax: true // optional
+            },
+            // Requires >= sass-loader@^8.0.0
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                indentedSyntax: true // optional
+              },
+            },
+          },
+        ],
+      },
+    ],
+  }
+})
+
 module.exports = environment
