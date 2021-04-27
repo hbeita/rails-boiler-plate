0b954888dfc03314145b191c0ff5e82145087f10
Hector-Beita <hector.garcia@shareworks.com>
2021-04-26 23:14:33 -0600

    Add schema changes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_021100) do
+ActiveRecord::Schema.define(version: 2021_04_27_051035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "membership_church_services", force: :cascade do |t|
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
