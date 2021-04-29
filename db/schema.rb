7ea577b5a3c9dcb892f97aab900ccc963edd6549
Hector-Beita <hector.garcia@shareworks.com>
2021-04-28 19:10:35 -0600

    Add membership attendee model.

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,36 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_427_052_540) do
+ActiveRecord::Schema.define(version: 2021_04_28_040721) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'membership_church_services', force: :cascade do |t|
-    t.date 'date'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "membership_church_service_attendees", force: :cascade do |t|
+    t.bigint "membership_church_service_id"
+    t.bigint "person_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["membership_church_service_id"], name: "idx_church_attendee_service"
+    t.index ["person_id"], name: "idx_church_attendee_person"
   end
 
-  create_table 'people', force: :cascade do |t|
-    t.string 'name'
-    t.string 'last_name'
-    t.string 'phone_number'
-    t.date 'birthday'
-    t.string 'address'
-    t.string 'type'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "membership_church_services", force: :cascade do |t|
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.date "birthday"
+    t.string "address"
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "membership_church_service_attendees", "membership_church_services"
+  add_foreign_key "membership_church_service_attendees", "people"
 end
---
+++ test/models/membership/church_service_attendee_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Membership::ChurchServiceAttendeeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
